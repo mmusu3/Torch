648a912bd1908b1e183d6a40961c8faef7e1d718
Michael Musumeci <mmusu3@live.com.au>
2024-02-16 17:47:11 +1000

    Cleanup launching and use custom assembly resolver instead of obsolete AppDomain private paths

--- Torch/Utils/TorchAssemblyResolver.cs
+++ Torch/Utils/TorchAssemblyResolver.cs
@@ -1,96 +1,112 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using NLog;
 
-namespace Torch.Utils
+namespace Torch.Utils;
+
+/// <summary>
+/// Adds and removes an additional library path
+/// </summary>
+public class TorchAssemblyResolver : IDisposable
 {
+    static readonly Logger _log = LogManager.GetCurrentClassLogger();
+
+    static readonly string[] _fileExtensionsToCheck = { ".dll", ".exe" };
+
+    readonly Dictionary<string, Assembly?> _assemblies = new Dictionary<string, Assembly?>();
+    readonly string[] _binDirectories;
+    readonly string _removablePathPrefix;
+
     /// <summary>
-    /// Adds and removes an additional library path
+    /// Initializes an assembly resolver that looks at the given paths for assemblies
     /// </summary>
-    public class TorchAssemblyResolver : IDisposable
+    /// <param name="paths"></param>
+    public TorchAssemblyResolver(params string[] paths)
     {
-        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+        string location = Assembly.GetEntryAssembly()?.Location ?? GetType().Assembly.Location;
 
-        private readonly Dictionary<string, Assembly> _assemblies = new Dictionary<string, Assembly>();
-        private readonly string[] _paths;
-        private readonly string _removablePathPrefix;
+        if (location != null)
+            location = Path.GetDirectoryName(location) + Path.DirectorySeparatorChar;
 
-        /// <summary>
-        /// Initializes an assembly resolver that looks at the given paths for assemblies
-        /// </summary>
-        /// <param name="paths"></param>
-        public TorchAssemblyResolver(params string[] paths)
+        _removablePathPrefix = location ?? "";
+        _binDirectories = paths;
+
+        AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+    }
+
+    string SimplifyPath(string path)
+    {
+        return path.StartsWith(_removablePathPrefix) ? path.Substring(_removablePathPrefix.Length) : path;
+    }
+
+    Assembly? CurrentDomainOnAssemblyResolve(object? sender, ResolveEventArgs args)
+    {
+        string assemblyName = new AssemblyName(args.Name).Name!;
+
+        lock (_assemblies)
         {
-            string location = Assembly.GetEntryAssembly()?.Location ?? GetType().Assembly.Location;
-            location = location != null ? Path.GetDirectoryName(location) + Path.DirectorySeparatorChar : null;
-            _removablePathPrefix = location ?? "";
-            _paths = paths;
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
-        }
+            Assembly? asm;
 
-        private string SimplifyPath(string path)
-        {
-            return path.StartsWith(_removablePathPrefix) ? path.Substring(_removablePathPrefix.Length) : path;
-        }
+            if (_assemblies.TryGetValue(assemblyName, out asm))
+                return asm;
 
-        private static readonly string[] _tryExtensions = {".dll", ".exe"};
-
-        private Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            string assemblyName = new AssemblyName(args.Name).Name;
-            lock (_assemblies)
-                if (_assemblies.TryGetValue(assemblyName, out Assembly asm))
-                    return asm;
-            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
-                if (asm.GetName().Name.Equals(assemblyName))
-                {
-                    lock (_assemblies)
-                        _assemblies.Add(assemblyName, asm);
-                    return asm;
-                }
-            lock (_assemblies)
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
             {
-                foreach (string path in _paths)
+                if (item.GetName().Name!.Equals(assemblyName))
                 {
-                    try
-                    {
-                        foreach (var tryExt in _tryExtensions)
-                        {
-                            string assemblyPath = Path.Combine(path, assemblyName + tryExt);
-                            if (!File.Exists(assemblyPath))
-                                continue;
-                            _log.Trace("Loading {0} from {1}", assemblyName, SimplifyPath(assemblyPath));
-                            LogManager.Flush();
-                            Assembly asm = Assembly.LoadFrom(assemblyPath);
-                            _assemblies.Add(assemblyName, asm);
-                            // Recursively load SE dependencies since they don't trigger AssemblyResolve.
-                            // This trades some performance on load for actually working code.
-                            foreach (AssemblyName dependency in asm.GetReferencedAssemblies())
-                                CurrentDomainOnAssemblyResolve(sender, new ResolveEventArgs(dependency.Name, asm));
-                            return asm;
-                        }
-                    }
-                    catch
-                    {
-                        // Ignored
-                    }
+                    _assemblies.Add(assemblyName, item);
+                    return item;
                 }
             }
-            return null;
+
+            foreach (string path in _binDirectories)
+            {
+                try
+                {
+                    foreach (var tryExt in _fileExtensionsToCheck)
+                    {
+                        string assemblyPath = Path.Combine(path, assemblyName + tryExt);
+
+                        if (!File.Exists(assemblyPath))
+                            continue;
+
+                        _log.Trace($"Loading {assemblyName} from {SimplifyPath(assemblyPath)}");
+
+                        LogManager.Flush();
+
+                        asm = Assembly.LoadFrom(assemblyPath);
+                        _assemblies.Add(assemblyName, asm);
+
+                        // Recursively load SE dependencies since they don't trigger AssemblyResolve.
+                        // This trades some performance on load for actually working code.
+                        //foreach (AssemblyName dependency in asm.GetReferencedAssemblies())
+                        //    CurrentDomainOnAssemblyResolve(sender, new ResolveEventArgs(dependency.Name, asm));
+
+                        return asm;
+                    }
+                }
+                catch
+                {
+                    // Ignored
+                }
+            }
         }
 
-        /// <summary>
-        /// Unregisters the assembly resolver
-        /// </summary>
-        public void Dispose()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomainOnAssemblyResolve;
-            _assemblies.Clear();
-        }
+        _assemblies.Add(assemblyName, null);
+
+        return null;
+    }
+
+    /// <summary>
+    /// Unregisters the assembly resolver
+    /// </summary>
+    public void Dispose()
+    {
+        AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomainOnAssemblyResolve;
+        _assemblies.Clear();
     }
 }
