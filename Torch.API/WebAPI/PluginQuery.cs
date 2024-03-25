9bf8d2ba0e3a35e24689a81c246a86c7a1295cf2
Michael Musumeci <mmusu3@live.com.au>
2024-03-26 03:22:07 +1000

    Set PooledConnectionLifetime to fix IOExceptions occuring from use of HttpClient in PluginQuery. Use ConfigureAwait(false). Dispose http responses and streams.

--- Torch.API/WebAPI/PluginQuery.cs
+++ Torch.API/WebAPI/PluginQuery.cs
@@ -21,16 +21,20 @@ namespace Torch.API.WebAPI
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         private static PluginQuery _instance;
-        public static PluginQuery Instance => _instance ?? (_instance = new PluginQuery());
+        public static PluginQuery Instance => _instance ??= new PluginQuery();
 
         private PluginQuery()
         {
+#if NET8_0_OR_GREATER
+            _client = new HttpClient(new SocketsHttpHandler { PooledConnectionLifetime = TimeSpan.FromSeconds(10) });
+#else
             _client = new HttpClient();
+#endif
         }
 
         public async Task<PluginResponse> QueryAll()
         {
-            var h = await _client.GetAsync(ALL_QUERY);
+            using var h = await _client.GetAsync(ALL_QUERY).ConfigureAwait(false);
 
             if (!h.IsSuccessStatusCode)
             {
@@ -38,9 +42,10 @@ namespace Torch.API.WebAPI
                 return null;
             }
 
-            var r = await h.Content.ReadAsStringAsync();
+            var r = await h.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             PluginResponse response;
+
             try
             {
                 response = JsonConvert.DeserializeObject<PluginResponse>(r);
@@ -50,27 +55,29 @@ namespace Torch.API.WebAPI
                 Log.Error(ex, "Failed to deserialize plugin query response!");
                 return null;
             }
+
             return response;
         }
 
-        public async Task<PluginFullItem> QueryOne(Guid guid)
+        public Task<PluginFullItem> QueryOne(Guid guid)
         {
-            return await QueryOne(guid.ToString());
+            return QueryOne(guid.ToString());
         }
 
         public async Task<PluginFullItem> QueryOne(string guid)
         {
+            using var h = await _client.GetAsync(string.Format(PLUGIN_QUERY, guid)).ConfigureAwait(false);
 
-            var h = await _client.GetAsync(string.Format(PLUGIN_QUERY, guid));
             if (!h.IsSuccessStatusCode)
             {
                 Log.Error($"Plugin query returned response {h.StatusCode}");
                 return null;
             }
 
-            var r = await h.Content.ReadAsStringAsync();
+            var r = await h.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             PluginFullItem response;
+
             try
             {
                 response = JsonConvert.DeserializeObject<PluginFullItem>(r);
@@ -80,53 +87,63 @@ namespace Torch.API.WebAPI
                 Log.Error(ex, "Failed to deserialize plugin query response!");
                 return null;
             }
+
             return response;
         }
 
-        public async Task<bool> DownloadPlugin(Guid guid, string path = null)
+        public Task<bool> DownloadPlugin(Guid guid, string path = null)
         {
-            return await DownloadPlugin(guid.ToString(), path);
+            return DownloadPlugin(guid.ToString(), path);
         }
 
         public async Task<bool> DownloadPlugin(string guid, string path = null)
         {
-            var item = await QueryOne(guid);
-            if (item == null) return false;
-            return await DownloadPlugin(item, path);
+            var item = await QueryOne(guid).ConfigureAwait(false);
+
+            if (item == null)
+                return false;
+
+            return await DownloadPlugin(item, path).ConfigureAwait(false);
         }
 
         public async Task<bool> DownloadPlugin(PluginFullItem item, string path = null)
         {
             try
             {
-                path = path ?? $"Plugins\\{item.Name}.zip";
+                path ??= $"Plugins\\{item.Name}.zip";
+
                 string relpath = Path.GetDirectoryName(path);
 
                 Directory.CreateDirectory(relpath);
 
-                var h = await _client.GetAsync(string.Format(PLUGIN_QUERY, item.ID));
-                string res = await h.Content.ReadAsStringAsync();
+                using var h = await _client.GetAsync(string.Format(PLUGIN_QUERY, item.ID)).ConfigureAwait(false);
+
+                string res = await h.Content.ReadAsStringAsync().ConfigureAwait(false);
                 var response = JsonConvert.DeserializeObject<PluginFullItem>(res);
+
                 if (response.Versions.Length == 0)
                 {
                     Log.Error($"Selected plugin {item.Name} does not have any versions to download!");
                     return false;
                 }
+
                 var version = response.Versions.FirstOrDefault(v => v.Version == response.LatestVersion);
+
                 if (version == null)
                 {
                     Log.Error($"Could not find latest version for selected plugin {item.Name}");
                     return false;
                 }
-                var s = await _client.GetStreamAsync(version.URL);
 
-                if(File.Exists(path))
+                using var s = await _client.GetStreamAsync(version.URL).ConfigureAwait(false);
+
+                if (File.Exists(path))
                     File.Delete(path);
 
                 using (var f = File.Create(path))
                 {
-                    await s.CopyToAsync(f);
-                    await f.FlushAsync();
+                    await s.CopyToAsync(f).ConfigureAwait(false);
+                    await f.FlushAsync().ConfigureAwait(false);
                 }
             }
             catch (Exception ex)
