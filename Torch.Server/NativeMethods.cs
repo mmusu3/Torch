d2109f41dc59cff30c57a6a48d818bd288f4b8bf
Michael Musumeci <mmusu3@live.com.au>
2024-04-08 13:08:09 +1000

    Cleanup NativeMethods class

--- Torch.Server/NativeMethods.cs
+++ Torch.Server/NativeMethods.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
-namespace Torch.Server
+namespace Torch.Server;
+
+public static class NativeMethods
 {
-    public class NativeMethods
-    {
-        [DllImport("kernel32")]
-        public static extern bool AllocConsole();
+    [DllImport("kernel32")]
+    public static extern bool AllocConsole();
 
-        [DllImport("kernel32")]
-        public static extern bool FreeConsole();
-    }
+    [DllImport("kernel32")]
+    public static extern nint GetConsoleWindow();
+
+    [DllImport("kernel32")]
+    public static extern bool FreeConsole();
 }
