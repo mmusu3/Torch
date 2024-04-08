13f7fa99c933869b4337ffc3fb9246c98ea8f6b5
Michael Musumeci <mmusu3@live.com.au>
2024-04-08 13:44:24 +1000

    Don't try to update Torch when current version is 0,0,0

--- Torch.Server/Managers/UpdateManager.cs
+++ Torch.Server/Managers/UpdateManager.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.IO.Packaging;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 using NLog;
 using Torch.API;
 using Torch.API.WebAPI;
@@ -26,8 +18,9 @@ namespace Torch.Managers
         private Logger _log = LogManager.GetCurrentClassLogger();
         private FilesystemManager _fsManager;
         private bool _isProcessing = false;
-        
-        public UpdateManager(ITorchBase torchInstance) : base(torchInstance)
+
+        public UpdateManager(ITorchBase torchInstance)
+            : base(torchInstance)
         {
             //fs is only really used here so might as well create it here
             _fsManager = new FilesystemManager(torchInstance);
@@ -38,43 +31,46 @@ namespace Torch.Managers
         {
         }
 
-        private void TimerElapsed(object state)
-        {
-        }
-        
         public async void CheckAndUpdateTorch()
         {
-            if (Torch.Config.NoUpdate || !Torch.Config.GetTorchUpdates || (Torch.Config.BranchName == TorchBranchType.dev))
-                return;
-
 #if  DEBUG
-            return;  
+            return;
 #endif
+
+            if (Torch.Config.NoUpdate
+                || !Torch.Config.GetTorchUpdates
+                || (Torch.Config.BranchName == TorchBranchType.dev)
+                || Torch.TorchVersion.Version == new Version(0, 0, 0))
+                return;
 
             try
             {
                 _log.Info("Checking for Torch Update...");
                 _isProcessing = true;
+
                 var job = await JenkinsQuery.Instance.GetLatestVersion(Torch.Config.BranchName.ToString());
+
                 if (job == null)
                 {
                     _log.Info("Failed to fetch latest version.");
                     return;
                 }
-                
+
                 _log.Info($"Latest version is {job.Version} | Current version is {Torch.TorchVersion}");
-                
+
                 if (job.Version > Torch.TorchVersion || (Torch.TorchVersion.Branch != Torch.Config.BranchName.ToString()))
                 {
                     _log.Warn($"Updating Torch from version {Torch.TorchVersion} to version {job.Version}");
                     var updateName = Path.Combine(_fsManager.TempDirectory, "torchupdate.zip");
                     //new WebClient().DownloadFile(new Uri(releaseInfo.Item2), updateName);
+
                     if (!await JenkinsQuery.Instance.DownloadRelease(job, updateName))
                     {
                         _log.Warn("Failed to download new release!");
                         _isProcessing = false;
                         return;
                     }
+
                     UpdateFromZip(updateName, _torchDir);
                     File.Delete(updateName);
                     _log.Warn($"Torch version {job.Version} has been installed.  Restarting...");
@@ -84,6 +80,7 @@ namespace Torch.Managers
                 {
                     _log.Info("Torch is up to date.");
                 }
+
                 _isProcessing = false;
 
             }
@@ -101,7 +98,7 @@ namespace Torch.Managers
             {
                 foreach (var file in zip.Entries)
                 {
-                    if(file.Name == "NLog-user.config" && File.Exists(Path.Combine(extractPath, file.FullName)))
+                    if (file.Name == "NLog-user.config" && File.Exists(Path.Combine(extractPath, file.FullName)))
                         continue;
 
                     var targetFile = Path.Combine(extractPath, file.FullName);
@@ -114,7 +111,7 @@ namespace Torch.Managers
                 //zip.ExtractToDirectory(extractPath); //throws exceptions sometimes?
             }
         }
-        
+
         /// <summary>
         /// Returns true if update data is being checked or processed.
         /// </summary>
